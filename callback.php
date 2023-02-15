41d278f3cf61ad4475c83cdfa4da5ddf66ac2b91
Rico N <info@rico-neumann.info>
2023-02-16 09:14:47 +1100

    Put optional parameter after required parameter

--- callback.php
+++ callback.php
@@ -11,7 +11,7 @@ if (isset($_REQUEST['code'])) {
 	$keys['code'] = $_REQUEST['code'];
 	$keys['redirect_uri'] = WB_CALLBACK_URL;
 	try {
-		$token = $o->getAccessToken( 'code', $keys ) ;
+		$token = $o->getAccessToken( $keys ) ;
 	} catch (OAuthException $e) {
 	}
 }
